8e2d7a8657d3e4d3f23eb93ea06f14a1084bb7fa
GiovannaKiarha <giovannakiarha@outlook.com>
2025-01-28 08:55:09 -0300

    Add files via upload

---
+++ desempenho.py
@@ -0,0 +1,12 @@
+# desempenho
+
+nota = int(input('Digite a nota do aluno: '))
+
+if nota >= 9:
+    print('Excelente, você tirou um A')
+elif nota >=7:
+    print('Bom trabalho, você tirou um B')
+elif nota >=5:
+    print('Você passou, mas precisa melhorar a sua nota')
+else:
+    print('Você foi reprovado')
